8665178db3a8c596034405507cac57eeb5e8a560
Kirill <wakened90@gmail.com>
2023-02-16 16:09:21 +0300

    NewStringsArrayCreation working

---
+++ Program code/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] NewStringsArrayCreation()
+{
+    int i = 0;
+
+    Console.Write("Введите размер массива строк: ");
+    int elementsCount = int.Parse(Console.ReadLine()!);
+    
+    if (elementsCount < 1)
+    {
+        Console.WriteLine("Введите целое положительное число");
+    }
+        
+    string[] array = new string[elementsCount];
+
+    while (i < array.Length)
+    {
+        Console.Write($"Введите {i + 1} строку: ");
+        array[i] = Console.ReadLine()!;
+        if (array[i] != "")
+        {
+            i++;
+        }    
+    }
+    
+    return array;
+}
